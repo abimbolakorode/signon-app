7c44c0af786f5eccf08323329b1ff67125557ad6
abimbolakorode <abimbolakorode21@gmail.com>
2024-08-08 15:36:30 -0700

    error done

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 
 import './App.css'
-import LoginPage from './components/login/LoginPage'
+// import LoginPage from './components/login/LoginPage'//
 
 import OtpPage from './components/otp/OtpPage'
 import RegisterPage from './components/register/RegisterPage'
 import ResetPasswordPage from './components/resetPassword/ResetpasswordPage'
+
 
 function App() {
     return (
   
       <div className='w-full h-screen bg'>
         <RegisterPage />
-        <LoginPage />
+        {/* <LoginPage  /> */}
         <ResetPasswordPage />
         <OtpPage />
     </div>
